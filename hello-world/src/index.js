f8c3efc11ee6322a83b2f1171d2e77c559a542bc
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-10-15 22:08:09 -0700

    Code from tutorial that produces Hello World on a browser.

---
+++ hello-world/src/index.js
@@ -0,0 +1,7 @@
+/** First React App. Tutorial: https://www.youtube.com/watch?v=Ke90Tje7VS0 **/
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const element = <h1> Hello World </h1>;
+
+ReactDom.render(element, document.getElementById('root'));
