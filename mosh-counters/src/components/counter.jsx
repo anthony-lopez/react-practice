6ee83cf1f0287dae8c1ccd76ec14dd80602176fa
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-10-21 23:18:42 -0700

    Added an Event Handler

--- mosh-counters/src/components/counter.jsx
+++ mosh-counters/src/components/counter.jsx
@@ -5,18 +5,34 @@ class Counter extends Component {
     count: 0
   };
 
+  // constructor() {
+  //   super();
+  //   //console.log("Constructor", this);
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  //arrow function instead of constructor; inherits 'this'
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
