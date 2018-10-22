8c288051e5b0c97376979f92b985b7590d6631ff
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-10-21 23:30:34 -0700

    Added ability to pass in arguments

--- mosh-counters/src/components/counter.jsx
+++ mosh-counters/src/components/counter.jsx
@@ -12,8 +12,13 @@ class Counter extends Component {
   // }
 
   //arrow function instead of constructor; inherits 'this'
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -21,7 +26,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
