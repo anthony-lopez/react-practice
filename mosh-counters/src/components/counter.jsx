0ccea50ca472f4bde5269e58cfb77049eb51c404
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-10-21 22:48:31 -0700

    Added styles

--- mosh-counters/src/components/counter.jsx
+++ mosh-counters/src/components/counter.jsx
@@ -5,11 +5,19 @@ class Counter extends Component {
     count: 0
   };
 
+  // styles = {
+  //   fontSize: 10,
+  //   fontWeight: "bold"
+  // };
+
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* this.styles */}
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
